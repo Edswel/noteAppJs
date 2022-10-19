ca879271ced9710d5211966be41dc8acf91d0edd
Edswel <franckini@yahoo.com>
2022-10-19 07:08:59 +0100

    Add show form functionality

---
+++ app.js
@@ -0,0 +1,6 @@
+const addSection = document.querySelector('.add-section');
+const addNew = document.querySelector('.add-new');
+
+addSection.addEventListener("click", () => {
+  addNew.classList.add("show");
+});
