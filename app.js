c747596bfe5a5eb9dacd528127a1746a4c6c7f7c
Edswel <franckini@yahoo.com>
2022-10-19 07:19:54 +0100

    Add close form functionality

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const addSection = document.querySelector('.add-section');
 const addNew = document.querySelector('.add-new');
+const closeIcon = addNew.querySelector('header i');
 
+// Add class to show form
 addSection.addEventListener("click", () => {
   addNew.classList.add("show");
+});
+
+// Remove close to close form
+closeIcon.addEventListener("click", () => {
+  addNew.classList.remove("show");
 });
