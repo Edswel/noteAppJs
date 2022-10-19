391c9135b828b7d993e35b22a69dc643bbb4c42d
Edswel <franckini@yahoo.com>
2022-10-19 07:48:23 +0100

    Add functionality to add new note and current time

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const addSection = document.querySelector('.add-section');
 const addNew = document.querySelector('.add-new');
 const closeIcon = addNew.querySelector('header i');
+const noteTitle = addNew.querySelector('input');
+const noteBody = addNew.querySelector('textarea');
+const addBtn = addNew.querySelector('button');
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 // Add class to show form
 addSection.addEventListener("click", () => {
@@ -10,4 +15,25 @@ addSection.addEventListener("click", () => {
 // Remove close to close form
 closeIcon.addEventListener("click", () => {
   addNew.classList.remove("show");
+});
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let newTitle = noteTitle.value;
+  let newBody = noteBody.value;
+
+  // Get current date
+  if (newTitle, newBody) {
+    let newDate = new Date();
+    let month = months[newDate.getMonth()];
+    let day = newDate.getDate();
+    let year = newDate.getFullYear();
+
+    let newNote = {
+      title: newTitle,
+      body: newBody,
+      date: `${month} ${day}, ${year}`
+    }
+    console.log(newNote);
+  }
 });
